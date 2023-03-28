6cace534acbdf57d86864a180f60998ba0fb77e5
JoaquinLazaro <93229748+JoaquinLazaro26@users.noreply.github.com>
2023-03-27 21:48:42 -0500

    Creation Project of Class

---
+++ Spring-Clinica/src/main/java/net/clinica/entity/TipoMedicamento.java
@@ -0,0 +1,46 @@
+package net.clinica.entity;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name="tb_tipo_medicamento")
+public class TipoMedicamento {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="cod_tipo")
+	private Integer codigo;
+	@Column(name="nom_tipo")
+	private String nombre;
+	@OneToMany(mappedBy = "tipo")
+	private List<Medicamento> listaMedicamentos;
+	
+	public Integer getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public List<Medicamento> getListaMedicamentos() {
+		return listaMedicamentos;
+	}
+	public void setListaMedicamentos(List<Medicamento> listaMedicamentos) {
+		this.listaMedicamentos = listaMedicamentos;
+	}
+	
+	
+	
+}
